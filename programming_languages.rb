b037d64ec837d13d138643708a65149c4a0e42f4
Yong Nam Kang <j.yongnamkang@gmail.com>
2019-01-30 01:12:40 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,8 +3,12 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |k, v|
     v.each do |lang, type|
-      binding.pry
-      new_hash[lang] = type
+      if new_hash[lang] == nil || new_hash[lang][:style] == nil
+        new_hash[lang] = type
+        new_hash[lang][:style] = [k]
+      else
+        new_hash[lang][:style] << k
+      end
     end
   end
   new_hash
